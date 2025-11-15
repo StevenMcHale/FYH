6cfbf0ff93afa288542837a6d01d54733f3a4b14
StevenMcHale <steven.mchale@icloud.com>
2025-11-15 23:46:26 +0000

    vercelfix

--- uni_planner/uni_planner/settings.py
+++ uni_planner/uni_planner/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,13 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-znf5fdg658#jjwyz!k7)(%1hr+am_5q1aj#l^3p(6s4f(%kf*f')
+SECRET_KEY = 'django-insecure-n-#s&$h7wt09db#t00d4aa8*g3t$yptf+i2b14+2^me&7&(r0&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', 'False') == 'True'
+DEBUG = True
 
-# Allow Vercel host plus localhost by default; in production set ALLOWED_HOSTS explicitly
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '*.vercel.app,localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app', '.now.sh']
 
 
 # Application definition
@@ -44,7 +42,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -77,32 +74,17 @@ WSGI_APPLICATION = 'uni_planner.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASE_URL = os.getenv('DATABASE_URL')
-if DATABASE_URL:
-    try:
-        import dj_database_url
-
-        DATABASES = {
-            'default': dj_database_url.parse(DATABASE_URL, conn_max_age=600),
-        }
-    except Exception:
-        DATABASES = {
-            'default': {
-                'ENGINE': 'django.db.backends.postgresql',
-                'NAME': os.getenv('PGNAME', 'postgres'),
-                'USER': os.getenv('PGUSER', ''),
-                'PASSWORD': os.getenv('PGPASSWORD', ''),
-                'HOST': os.getenv('PGHOST', ''),
-                'PORT': os.getenv('PGPORT', '5432'),
-            }
-        }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres.labihsrujpchbcvtszao',
+        'PASSWORD': 'kcl12345',
+        'HOST': 'aws-1-eu-north-1.pooler.supabase.com',
+        'PORT': '5432',
+        'CONN_MAX_AGE': 0,
     }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
@@ -139,8 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
