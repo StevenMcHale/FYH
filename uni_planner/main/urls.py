d715486ba2fe0eb9766c78feba4e0022a90a2534
StevenMcHale <steven.mchale@icloud.com>
2025-11-15 14:25:40 +0000

    login+register

--- uni_planner/main/urls.py
+++ uni_planner/main/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.home, name="home"),
     path('meals/', views.meals, name="meals"),
-    path('chores', views.chores, name="chores"),
+    path('chores/', views.chores, name="chores"),
+    path('register/', views.registerPage, name="register"),
+    path('login/', views.loginPage, name="login"),
+    path('logout/', views.logoutPage, name="logout"),
 ]
